09e21605c1e98151e31565b2675e2f08895efae5
ewen <ewen.grosjean@free.fr>
2017-06-18 17:33:19 +0200

    more controls

--- src/muancefx/h.clj
+++ src/muancefx/h.clj
@@ -2,10 +2,17 @@
   (:require [muancefx.core :as m]))
 
 (def javafx-nodes {'stack-pane 'javafx.scene.layout.StackPane
+                   'anchor-pane 'javafx.scene.layout.AnchorPane
+                   'v-box 'javafx.scene.layout.VBox
+                   'h-box 'javafx.scene.layout.HBox
+                   'checkbox 'javafx.scene.control.CheckBox
                    'button 'javafx.scene.control.Button
                    'text-field 'javafx.scene.control.TextField
                    'rectangle 'javafx.scene.shape.Rectangle
-                   'text 'javafx.scene.text.Text})
+                   'text 'javafx.scene.text.Text
+                   'label 'javafx.scene.control.Label
+                   'list-view 'javafx.scene.control.ListView
+                   'toggle-button 'javafx.scene.control.ToggleButton})
 
 (def element-macros javafx-nodes)
 
